bc96a783b332bf220cb67ff7e9c84fc2a5e6e523
SAE1\1027859 <1027859@student.sae.edu.au>
2023-07-17 12:12:28 +1000

    basic platformer movement + experimentation

---
+++ Assets/MeshAnimator.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshAnimator : MonoBehaviour
+{
+    public MeshFilter meshFilter;
+    public GameObject[] meshes;
+    float timer;
+    public float maxTimePerFrame;
+    int no;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        meshFilter.mesh = meshes[no].GetComponentInChildren<MeshFilter>().sharedMesh;
+        timer += Time.deltaTime;
+
+        if (timer >= maxTimePerFrame)
+        {
+            timer = 0;
+            if (no < meshes.Length-1)
+            {
+                no += 1;
+            }
+        }
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    public float jumpSpeed;
+    public float mouseSpeed;
+    Rigidbody rb;
+
+    bool jumping;
+    float jumpTimer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.forward * Input.GetAxis("Vertical") * speed * Time.deltaTime + transform.right * Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+
+        transform.Rotate(0, Input.GetAxis("Mouse X") * mouseSpeed, 0);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jumping = true;
+        }
+
+        if (Input.GetKey(KeyCode.Space) && jumping)
+        {
+            rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+            transform.position += transform.up * jumpSpeed * Time.deltaTime;
+        }
+
+        if (jumping)
+        {
+            jumpTimer += Time.deltaTime;
+
+            if (jumpTimer >= 0.25f)
+            {
+                jumpTimer = 0;
+                jumping = false;
+            }
+        }
+    }
+}
