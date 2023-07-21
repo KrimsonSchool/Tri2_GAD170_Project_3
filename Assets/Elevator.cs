cc77de0626b56caaf4bbb667ec6993b3bca885bf
JAIS_PC\jaivd <jaivda111@gmail.com>
2023-07-21 23:53:19 +1000

    added more stuff

---
+++ Assets/Elevator.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elevator : MonoBehaviour
+{
+    public Vector3 startPos;
+    public Vector3 endPos;
+
+    public MeshRenderer meshRenderer;
+    public Material defaultMat;
+    public Material altMat;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
