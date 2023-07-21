c864c6fa6e19e5d2b36396850880d33b3eff71f6
JAIS_PC\jaivd <jaivda111@gmail.com>
2023-07-22 00:08:45 +1000

    elevator

--- Assets/Elevator.cs
+++ Assets/Elevator.cs
@@ -10,6 +10,10 @@ public class Elevator : MonoBehaviour
     public MeshRenderer meshRenderer;
     public Material defaultMat;
     public Material altMat;
+
+    public float speed;
+
+    bool moving;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +23,24 @@ public class Elevator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (moving && transform.position != endPos)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, endPos, Time.deltaTime * speed);
+        }
+        else if(Vector3.Distance(transform.position, endPos) < 1)
+        {
+            moving = false;
+        }
+
+        if(!moving && transform.position != startPos)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, startPos, Time.deltaTime * speed);
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        moving = true;
     }
 }
